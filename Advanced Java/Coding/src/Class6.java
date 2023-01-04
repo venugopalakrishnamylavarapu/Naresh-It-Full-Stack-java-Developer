c883a2ae5c799b8a0d33a07c45a51c8172b55c78
Krishna <venugopalakrishnamylavarapu@gmail.com>
2023-01-04 20:59:24 +0530

    New files have been added

---
+++ Advanced Java/Coding/src/Class6.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+public class Class6 {
+    public static void main(String[] args) {
+        try{
+            Class c=Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con= DriverManager.getConnection("jdbc::oracle:oci8:@SYSTEM", "system", "tiger");
+        }catch(Exception e)
+        {
+            System.err.println(e);
+        }
+    }
+}
