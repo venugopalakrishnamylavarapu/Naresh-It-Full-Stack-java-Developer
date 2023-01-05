890775d738fe9c64a803b88a10ff584ae4d21650
Krishna <venugopalakrishnamylavarapu@gmail.com>
2023-01-05 21:27:41 +0530

    files have been adde

--- Advanced Java/Coding/src/Class6.java
+++ Advanced Java/Coding/src/Class6.java
@@ -5,7 +5,7 @@ public class Class6 {
     public static void main(String[] args) {
         try{
             Class c=Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con= DriverManager.getConnection("jdbc::oracle:oci8:@SYSTEM", "system", "tiger");
+            Connection con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:SYSTEM", "system", "tiger");
         }catch(Exception e)
         {
             System.err.println(e);
